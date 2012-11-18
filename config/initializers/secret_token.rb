57c77183cd97da35e9bf1844657e7d49a62ee930
jean <sjeanc911@gmail.com>
2012-11-18 12:45:03 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linky::Application.config.secret_token = '96c25778134c2ae2d746efc9ec0d093f3efcedfe17016e19b0e15ad5155ae54e04fb5662b932d37d9f221f0a3819ae086ec1409d647ad0abe5492b49a29a4a01'
